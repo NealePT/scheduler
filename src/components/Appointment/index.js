26ad153cf489f46fa57ff79a7d4ef3700caa220f
Neale Taylor <nealeptaylor@gmail.com>
2022-02-23 16:43:29 -0500

    add confirm component and story

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
+import Confirm from "components/Appointment/Confirm.js"
 
 
 export default function Appointment(props) {
