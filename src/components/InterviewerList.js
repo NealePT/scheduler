708d61558cd911428f06b2994440b5a5b9479c44
Neale Taylor <nealeptaylor@gmail.com>
2022-02-23 15:05:39 -0500

    add functionality to interviewlist

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "components/InterviewerList.scss";
-import "components/InterviewerListItem"
+import InterviewerListItem from "./InterviewerListItem";
 
 
 export default function InterviewerList(props) {
+  const interviewerInfo = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem 
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={event => props.setInterviewer(interviewer.id)}
+      />
+    )
+  })
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {interviewerInfo}
+      </ul>
     </section>
   )
 }
