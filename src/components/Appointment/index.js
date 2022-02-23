6d4a2ab0662b45466f1b1c432f38b6a7d17a6ddd
Neale Taylor <nealeptaylor@gmail.com>
2022-02-23 16:37:36 -0500

    add components to appointment

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
+
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment"></article>
+  );
+};
