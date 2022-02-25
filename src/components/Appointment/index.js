5da508ecd7b5d747b49a96ae3660b12c9db68867
Neale Taylor <nealeptaylor@gmail.com>
2022-02-25 16:06:20 -0500

    add functionality to use hook in application- only some features so far

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,13 +7,41 @@ import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
 import Error from "components/Appointment/Error.js";
 import Form from "components/Appointment/Form";
+import useVisualMode from 'hooks/useVisualMode';
+import { eqProps } from 'ramda';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE"
+
 
 
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+  props.interview ? SHOW : EMPTY
+);
+
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty/>}
+      {mode === EMPTY && (
+        <Empty 
+          onAdd={() => transition(CREATE)} 
+        />
+      )}
+      {mode === SHOW && (
+        <Show 
+          student={props.interview.student} 
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form 
+          interviewers={props.interviewers}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   );
 };
